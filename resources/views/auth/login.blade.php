a8283ea77e085590ed6c3cfacf480437ab3d7461
S. Sadrul Hossain <sadrul.hossain@daraz.com.bd>
2024-01-24 23:36:22 +0600

    gd-invms first post

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -49,64 +49,62 @@
     <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
     <script src="{{asset('assets/js/config.js')}}"></script>
+    <style>
+        .login{
+
+        }
+        .login-container{
+            background: url(../../../../img/login-bg.jpeg) no-repeat center center fixed !important;
+            background-size: cover !important;
+        }
+    </style>
 </head>
 
 <body>
 <!-- Content -->
+<div class="login">
+    <div class="login-container">
+        <div class="container-xxl">
+            <div class="authentication-wrapper authentication-basic container-p-y">
+                <div class="authentication-inner">
+                    <!-- Register -->
+                    <div class="card">
+                        <div class="card-body">
+                            <!-- Logo -->
+                            <div class="app-brand justify-content-center">
+                                <a href="{{url('/')}}" class="app-brand-link gap-2">
+                                    <span class="app-brand-text demo text-body fw-bolder">
+                                        GD Inventory Management System
+                                    </span>
+                                </a>
+                            </div>
+                            <!-- /Logo -->
 
-<div class="container-xxl">
-    <div class="authentication-wrapper authentication-basic container-p-y">
-        <div class="authentication-inner">
-            <!-- Register -->
-            <div class="card">
-                <div class="card-body">
-                    <!-- Logo -->
-                    <div class="app-brand justify-content-center">
-                        <a href="{{url('/')}}" class="app-brand-link gap-2">
-                            <span class="app-brand-text demo text-body fw-bolder">
-                                GD Inventory Management System
-                            </span>
-                        </a>
+                            {!! Form::open(['url' => route('login'), 'method' => 'POST', 'id' => 'formAuthentication', 'class' => 'mb-3']) !!}
+                            @csrf
+                                <div class="mb-3">
+                                    <label for="username" class="form-label">Username</label>
+                                    {!! Form::text('username', null, ['id' => 'username', 'class' => 'form-control', 'placeholder' => 'Enter your username', 'autofocus' => 'true']) !!}
+                                </div>
+                                <div class="mb-3 form-password-toggle">
+                                    <div class="d-flex justify-content-between">
+                                        <label class="form-label" for="password">Password</label>
+                                    </div>
+                                    <div class="input-group input-group-merge">
+                                        {!! Form::password('password', ['id' => 'password', 'class' => 'form-control', 'placeholder' => '&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;', 'area-describedby' => 'password']) !!}
+                                        <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                                    </div>
+                                </div>
+                                <div class="mb-3">
+                                    <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
+                                </div>
+                            {!! Form::close() !!}
+
+                        </div>
                     </div>
-                    <!-- /Logo -->
-
-                    {!! Form::open(['url' => route('login'), 'method' => 'POST']) !!}
-                    @csrf
-                        <div class="mb-3">
-                            <label for="username" class="form-label">Username</label>
-                            <input
-                                type="text"
-                                class="form-control"
-                                id="username"
-                                name="username"
-                                placeholder="Enter your username"
-                                autofocus
-                            />
-                        </div>
-                        <div class="mb-3 form-password-toggle">
-                            <div class="d-flex justify-content-between">
-                                <label class="form-label" for="password">Password</label>
-                            </div>
-                            <div class="input-group input-group-merge">
-                                <input
-                                    type="password"
-                                    id="password"
-                                    class="form-control"
-                                    name="password"
-                                    placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-                                    aria-describedby="password"
-                                />
-                                <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
-                            </div>
-                        </div>
-                        <div class="mb-3">
-                            <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
-                        </div>
-                    {!! Form::close() !!}
-
+                    <!-- /Register -->
                 </div>
             </div>
-            <!-- /Register -->
         </div>
     </div>
 </div>

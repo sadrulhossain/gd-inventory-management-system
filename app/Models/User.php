7738dc981b669b0b431b0d9ac375f28e8a2d8086
S. Sadrul Hossain <sadrul.hossain@daraz.com.bd>
2024-01-24 22:54:18 +0600

    gd-invms first post

---
+++ app/Models/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, HasRoles, Notifiable;
+
+    protected $fillable = [
+        'name', 'username', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+}
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Role::query()->insert([
+            ['title' => 'Admin', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['title' => 'Manager', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['title' => 'Store Manager', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['title' => 'Outlet Manager', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['title' => 'Top Authority', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+        ]);
+
+        DB::table('user_role')->insert([
+            'user_id' => 1,
+            'role_id' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+
+
+    }
+}
